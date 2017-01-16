e4d892549ffc60f992551665fee519c2c3ae9db9
thiagolucio <thiagolucio@gmail.com>
2017-01-16 17:27:10 -0200

    restarting project

--- app-angular2/app/primeiro/meu-primeiro.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'meu-primeiro-componente',
-    template: '<h3 class="title"> Meu Primeiro Componente</h3><p>Exemplo de inserção de componente simples tipo String com aspas simples</p>'
-})
-export class MeuPrimeiroComponent { }
